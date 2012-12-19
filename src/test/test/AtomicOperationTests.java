5552df0d25cb68d6cae3ed51d9cec81252ad56d9
Victor Ladochkin <victor.ladochkin@onlite.net>
2012-12-19 21:16:17 +0700

    Added tests for atomic operation.

--- src/test/test/AtomicOperationTests.java
+++ src/test/test/AtomicOperationTests.java
@@ -6,57 +6,142 @@ import com.google.code.morphia.query.UpdateOperations;
 import net.onlite.morplay.AtomicOperation;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.fest.assertions.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 /**
  * Atomic operation tests
  */
 public class AtomicOperationTests {
+
+    /**
+     * Test constants
+     */
+
+    public static final String TEST_FIELD_NAME          = "testFieldName";
+    public static final String TEST_FIELD_STRING_VALUE  = "testFieldStringValue";
+    public static final int TEST_FIELD_INT_VALUE        = 3;
+    public static final boolean TEST_UPSERT_VALUE       = true;
+    public static final List<String> TEST_ARRAY_VALUE   = Arrays.asList(
+            TEST_FIELD_STRING_VALUE,
+            TEST_FIELD_STRING_VALUE
+    );
+
+    /**
+     * Incapsulate mocked db classes
+     */
+    private static class DbMock<T> {
+        private final UpdateOperations<T> updateOperations;
+        private final Query<T> query;
+        private final Datastore ds;
+
+        @SuppressWarnings("unchecked")
+        private DbMock(Class<T> entityClass) {
+            updateOperations = (UpdateOperations<T>)mock(UpdateOperations.class);
+            query = (Query<T>)mock(Query.class);
+            ds = mock(Datastore.class);
+
+            when(query.getEntityClass()).thenReturn(entityClass);
+            when(ds.createUpdateOperations(entityClass)).thenReturn(updateOperations);
+        }
+
+        public UpdateOperations<T> getUpdateOperations() {
+            return updateOperations;
+        }
+
+        public Query<T> getQuery() {
+            return query;
+        }
+
+        public Datastore getDs() {
+            return ds;
+        }
+    }
+
     @Test
-    @SuppressWarnings("unchecked")
+    public void constructorTest() {
+        DbMock<String> dbMock = new DbMock<>(String.class);
+
+        // Create operation
+        AtomicOperation<String> testOp = createAtomicOperation(dbMock);
+
+        assertThat(testOp).isNotNull();
+        assertThat((Object)testOp.query()).isSameAs(testOp.query());
+        verify(dbMock.getDs(), times(1)).createUpdateOperations(String.class);
+    }
+
+    @Test
     public void setTest() {
-        UpdateOperations<String> updateOperations = (UpdateOperations<String>)mock(UpdateOperations.class);
-        Query<String> query = (Query<String>)mock(Query.class);
-        Datastore ds = mock(Datastore.class);
+        DbMock<String> dbMock = new DbMock<>(String.class);
+        AtomicOperation<String> testOp = createAtomicOperation(dbMock);
 
-        when(query.getEntityClass()).thenReturn(String.class);
-        when(ds.createUpdateOperations(String.class)).thenReturn(updateOperations);
-
-        AtomicOperation<String> testOp = new AtomicOperation<String>(ds, query, false);
-        testOp.set("testField", "testEntity");        
-                
-        verify(ds, only()).createUpdateOperations(String.class);
-        verify(updateOperations, only()).set("testField", "testEntity");
+        testOp.set(TEST_FIELD_NAME, TEST_FIELD_STRING_VALUE);
+        verify(dbMock.getUpdateOperations(), times(1)).set(TEST_FIELD_NAME, TEST_FIELD_STRING_VALUE);
     }
 
     @Test
     public void unsetTest() {
+        DbMock<String> dbMock = new DbMock<>(String.class);
+        AtomicOperation<String> testOp = createAtomicOperation(dbMock);
+
+        testOp.unset(TEST_FIELD_NAME);
+        verify(dbMock.getUpdateOperations(), times(1)).unset(TEST_FIELD_NAME);
 
     }
 
     @Test
     public void incTest() {
+        DbMock<String> dbMock = new DbMock<>(String.class);
+        AtomicOperation<String> testOp = createAtomicOperation(dbMock);
 
+        testOp.inc(TEST_FIELD_NAME, TEST_FIELD_INT_VALUE);
+        verify(dbMock.getUpdateOperations(), times(1)).inc(TEST_FIELD_NAME, TEST_FIELD_INT_VALUE);
     }
 
     @Test
     public void addTest() {
+        DbMock<String> dbMock = new DbMock<>(String.class);
+        AtomicOperation<String> testOp = createAtomicOperation(dbMock);
 
+        testOp.add(TEST_FIELD_NAME, TEST_FIELD_STRING_VALUE);
+        verify(dbMock.getUpdateOperations(), times(1)).add(TEST_FIELD_NAME, TEST_FIELD_STRING_VALUE, true);
+
+        testOp = createAtomicOperation(dbMock);
+        testOp.add(TEST_FIELD_NAME, TEST_ARRAY_VALUE);
+        verify(dbMock.getUpdateOperations(), times(1)).addAll(TEST_FIELD_NAME, TEST_ARRAY_VALUE, true);
     }
 
     @Test
     public void removeTest() {
+        DbMock<String> dbMock = new DbMock<>(String.class);
+        AtomicOperation<String> testOp = createAtomicOperation(dbMock);
 
+        testOp.remove(TEST_FIELD_NAME, TEST_FIELD_STRING_VALUE);
+        verify(dbMock.getUpdateOperations(), times(1)).removeAll(TEST_FIELD_NAME, TEST_FIELD_STRING_VALUE);
     }
 
     @Test
     public void popTest() {
+        DbMock<String> dbMock = new DbMock<>(String.class);
+        AtomicOperation<String> testOp = createAtomicOperation(dbMock);
 
+        testOp.pop(TEST_FIELD_NAME);
+        verify(dbMock.getUpdateOperations(), times(1)).removeFirst(TEST_FIELD_NAME);
     }
 
     @Test
     public void updateTest() {
+        DbMock<String> dbMock = new DbMock<>(String.class);
+        AtomicOperation<String> testOp = createAtomicOperation(dbMock);
 
+        testOp.update(TEST_UPSERT_VALUE);
+        verify(dbMock.getDs(), atLeastOnce()).updateFirst(dbMock.getQuery(), dbMock.getUpdateOperations(), TEST_UPSERT_VALUE);
     }
 
+    private <T> AtomicOperation<T> createAtomicOperation(DbMock<T> dbMock) {
+        return new AtomicOperation<>(dbMock.getDs(), dbMock.getQuery(), false);
+    }
 }
