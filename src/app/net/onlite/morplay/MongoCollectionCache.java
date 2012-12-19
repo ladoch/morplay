1a34cb0dc7373df326433cae8d5dd4ddafa503e0
Victor Ladochkin <victor.ladochkin@onlite.net>
2012-12-19 21:36:05 +0700

    Added some comments.

--- src/app/net/onlite/morplay/MongoCollectionCache.java
+++ src/app/net/onlite/morplay/MongoCollectionCache.java
@@ -13,16 +13,39 @@ public class MongoCollectionCache {
      * Cache
      */
     private final Map<String, MongoCollection> cache = new HashMap<>();
-    
+
+    /**
+     * Check whether cache contains requested collection or not
+     * @param entityClass Entity class
+     * @param ds Datastore instance
+     * @param <T> Entity type
+     * @return True or false.
+     */
     public <T> boolean contains(Class<T> entityClass, Datastore ds) {
         return cache.containsKey(key(entityClass, ds.getDB().getName()));
     }
-    
+
+    /**
+     * Add collection to cache
+     * @param entityClass Entity class
+     * @param ds Datastore instance
+     * @param collection Collection wrapper instance
+     * @param <T> Entity type.
+     * @return Cached collection
+     */
     public <T> MongoCollection<T> add(Class<T> entityClass, Datastore ds, MongoCollection<T> collection) {
         cache.put(key(entityClass, ds.getDB().getName()), collection);
         return collection;
     }
-    
+
+    /**
+     * Get collection from cache
+     * @param entityClass Entity class
+     * @param ds Datastore instance.
+     * @param resultClass Collection class
+     * @param <T> Entity type
+     * @return Collection instance or null if not cached.
+     */
     public <T> MongoCollection<T> get(Class<T> entityClass,
                                       Datastore ds,
                                       Class<? extends MongoCollection<T>> resultClass) {
@@ -34,6 +57,9 @@ public class MongoCollectionCache {
         return resultClass.cast(cache.get(key(entityClass, ds.getDB().getName())));
     }
 
+    /**
+     * Clear cache
+     */
     public void clear() {
         cache.clear();
     }
