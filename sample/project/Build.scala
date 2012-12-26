79ab6d5524597b3a63fd5a2991b1718cda6ccd13
Victor Ladochkin <victor.ladochkin@onlite.net>
2012-12-26 23:28:40 +0700

    Versions changed.

--- sample/project/Build.scala
+++ sample/project/Build.scala
@@ -5,11 +5,11 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "sample"
-  val appVersion      = "0.2"
+  val appVersion      = "0.3"
 
   val appDependencies = Seq(
     javaCore,
-    "morplay"   % "morplay_2.10" % "0.2"
+    "morplay"   % "morplay_2.10" % "0.3"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
